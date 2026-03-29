072f64b24d348fcb5cfa8968899b7003d986dab8
shreyashrangari08-gif <shreyashrangari08@gmail.com>
2026-03-29 22:38:13 +0530

    Update main.tsx

--- Src/main.tsx
+++ Src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.tsx' // Agar file ka naam app.tsx (chota 'a') hai
+import App from './App.tsx' // Agar file ka naam app.tsx (chota 'a') hai
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
